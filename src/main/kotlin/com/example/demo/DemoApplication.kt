5768ea813b34b36009c0e9b518f5ac99cee3605c
Daekeun Ko <kdkcom1234@gmail.com>
2023-11-23 15:32:57 +0900

    first commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,23 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+	@GetMapping
+	fun hello() : String {
+		return "Hello, Jenkins"
+	}
+}
